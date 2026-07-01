4b2a2e5de5599f45e8412860721a367fafb78c77
ChaoticPandaisavailable <zjc20050705@126.com>
2026-07-01 15:26:03 +0800

    Fix rotation state loss and page indicator

--- app/src/main/java/com/scanclone/MainActivity.kt
+++ app/src/main/java/com/scanclone/MainActivity.kt
@@ -1421,6 +1421,7 @@ class MainActivity : ComponentActivity() {
             } else {
                 movedIndex.coerceIn(0, pages.lastIndex)
             }
+            title.text = pageTitle()
             stripAdapter.selectedIndex = selectedPageIndex
             refreshPreview(preview, progress, maxLongEdge = mainPreviewLongEdge)
         }
